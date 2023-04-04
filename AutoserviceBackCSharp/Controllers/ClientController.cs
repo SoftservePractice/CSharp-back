686daa38389b8730cf40f7e17c7b1170f6c385e1
Crustbrns <georgejoof@gmail.com>
2023-04-04 23:28:25 +0300

    Small additions to Client controller

--- AutoserviceBackCSharp/Controllers/ClientController.cs
+++ AutoserviceBackCSharp/Controllers/ClientController.cs
@@ -17,36 +17,45 @@ namespace AutoserviceBackCSharp.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Client> GetClients(string? phone, string? email, string? telegramId)
+        public IEnumerable<Client> GetClients(string? name, string? phone, string? email, string? telegramId)
         {
             return _context.Clients.Where(
                 client =>
-                    (phone == null || client.Phone == phone)
+                    (name == null || client.Name == name)
+                    && (phone == null || client.Phone == phone)
                     && (email == null || client.Email == email)
                     && (telegramId == null || client.TelegramId == telegramId)
             )!;
         }
+
         [HttpGet("~/[controller]/{id}")]
         public Client GetClient(int id)
         {
             return _context.Clients.SingleOrDefault(client => client.Id == id)!;
         }
-        
+
         [HttpPost]
         public Client PostClient(string name, string phone, string? email, string? telegramId)
         {
-            var client = new Client() { Name = name, Phone = phone, Email = email ?? null, TelegramId = telegramId ?? null, IsConfirm = false };
+            var client = new Client()
+            {
+                Name = name,
+                Phone = phone,
+                Email = email ?? null,
+                TelegramId = telegramId ?? null,
+                IsConfirm = false
+            };
             _context.Clients.Add(client);
             _context.SaveChanges();
             return client;
         }
-        
+
         [HttpPatch("~/[controller]/{id}")]
         public bool UpdateClient(int id, string? name, string? phone, string? email, string? telegramId)
         {
             var client = _context.Clients.SingleOrDefault(client => client.Id == id);
 
-            if(client != null)
+            if (client != null)
             {
                 client.Name = name ?? client.Name;
                 client.Phone = phone ?? client.Phone;
@@ -58,13 +67,20 @@ namespace AutoserviceBackCSharp.Controllers
 
             return false;
         }
-        
+
         [HttpDelete("~/[controller]/{id}")]
         public bool DeleteClient(int id)
         {
-            _context.Remove(new Client() { Id = id });
-            _context.SaveChanges();
-            return true;
+            var client = _context.Clients.SingleOrDefault(client => client.Id == id);
+
+            if (client != null)
+            {
+                _context.Remove(client);
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
         }
     }
 }
