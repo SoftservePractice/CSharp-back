259c1e1a99fff1c30bae4bc5c81f958ff4890c44
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2023-04-04 23:37:30 +0300

    added WorkList controller

---
+++ AutoserviceBackCSharp/Controllers/WorkListController.cs
@@ -0,0 +1,75 @@
+using AutoserviceBackCSharp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoserviceBackCSharp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkListController : ControllerBase
+    {
+        private readonly ILogger<WorkListController> _logger;
+        private readonly PracticedbContext _context;
+
+        public WorkListController(ILogger<WorkListController> logger, PracticedbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<WorkList> GetWorkLists(string name, string? description, float? price, float? duration)
+        {
+            return _context.WorkLists.Where(
+                workList =>
+		    (name == null || workList.Name == name)
+                    && (description == null || workList.Description == description)
+                    && (price == null || workList.Price == price)
+                    && (duration == null || workList.Duration == duration)
+            )!;
+        }
+
+        [HttpGet("~/[controller]/{id}")]
+        public WorkList GetWorkList(int id)
+        {
+            return _context.WorkLists.SingleOrDefault(workList => workList.Id == id);
+        }
+
+        [HttpPost]
+        public WorkList PostWorkList(string name, string description, float price, float duration)
+        {
+	    var workList = new WorkList() { Name = name, Description = description, Price = price, Duration = duration };
+            _context.WorkLists.Add(workList);
+            _context.SaveChanges();
+            return workList;
+        }
+
+        [HttpPatch("~/[controller]/{id}")]
+        public bool UpdateWorkList(int id, string? name, string? description, float? price, float? duration)
+        {
+            var updWorkList = _context.WorkLists.SingleOrDefault(wl => wl.Id == id);
+	    if(updWorkList != null)
+            {
+            	updWorkList.Name = name ?? updWorkList.Name;
+            	updWorkList.Description = description ?? updWorkList.Description;
+            	updWorkList.Price = price ?? updWorkList.Price;
+            	updWorkList.Duration = duration ?? updWorkList.Duration;
+            	_context.SaveChanges();
+
+            	return true;
+	      }
+	     return false;
+        }
+
+        [HttpDelete("~/[controller]/{id}")]
+        public bool DeleteWorkList(int id)
+        {
+	    var workList = _context.WorkLists.SingleOrDefault(workList => workList.Id == id);
+
+            if (workList != null)
+            {
+            _context.Remove(new WorkList { Id = id });
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
