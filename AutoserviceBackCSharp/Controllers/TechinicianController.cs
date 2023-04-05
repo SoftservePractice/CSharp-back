820aeca4df70f92c113bbf4d7b7ad96e288be60a
bajenoB <jonjill1488@gmail.com>
2023-04-05 14:52:22 +0300

    Create TechicianController

---
+++ AutoserviceBackCSharp/Controllers/TechinicianController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoserviceBackCSharp.Controllers
+{
+    public class TechinicianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
