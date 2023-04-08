91e201c79f1413d7102d90231c0a5027ffc05530
bajenoB <jonjill1488@gmail.com>
2023-04-08 19:21:23 +0300

    Created validation class and update TechinicianController

--- AutoserviceBackCSharp/Controllers/TechinicianController.cs
+++ AutoserviceBackCSharp/Controllers/TechinicianController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AutoserviceBackCSharp.Models;
+using AutoserviceBackCSharp.Validation;
 
 namespace AutoserviceBackCSharp.Controllers
 {
@@ -10,6 +11,7 @@ namespace AutoserviceBackCSharp.Controllers
     {
         private readonly ILogger<TechinicianController> _logger;
         private readonly PracticedbContext _context;
+
 
         public TechinicianController(ILogger<TechinicianController> logger, PracticedbContext context)
         {
@@ -29,18 +31,85 @@ namespace AutoserviceBackCSharp.Controllers
         }
 
         [HttpPost]
-        public Technician PostTechnician(string name, string phone, string specialization, DateTime startWork, DateTime startWorkInCompany)
+        public ActionResult PostTechnician(string? name, string? phone, string? specialization, DateTime? startWork, DateTime? startWorkInCompany)
         {
-            var newTechnician = new Technician() { Name = name, Phone = phone, Specialization=specialization, StartWork = DateOnly.FromDateTime(startWork), StartWorkInCompany = DateOnly.FromDateTime(startWorkInCompany) };
+            
+            var validator = new SymbolValidator(new char[] { '%', '$', '@', '!', '%', '^', '`' });
+            if ((name == null || name.Length == 0) || (phone == null || phone.Length == 0)||(specialization==null||specialization.Length==0))
+            {
+                return BadRequest(
+                    new { message = "Имя или номер или специализация техника не могут быть пустыми" }
+                );
+            }
+
+            
+
+            if (!name.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("Имя техника может содержать только буквы");
+            }
+            if(validator.IsValid(specialization)==false)
+            {
+                return BadRequest("Специализация техника не может содержать такие символы");
+            }
+
+            var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+            try
+            {
+                var phoneNumber = phoneNumberUtil.Parse(phone, "UA");
+                if (!phoneNumberUtil.IsValidNumber(phoneNumber))
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Номер телефона должен быть корректным");
+            }
+
+            var newTechnician = new Technician() { Name = name, Phone = phone, Specialization=specialization, StartWork = DateOnly.FromDateTime((DateTime)startWork), StartWorkInCompany = DateOnly.FromDateTime((DateTime)startWorkInCompany) };
             _context.Technicians.Add(newTechnician);
             _context.SaveChanges();
-            return newTechnician;
+            return CreatedAtAction(nameof(PostTechnician), new { newTechnician = newTechnician, message = "Техник успешно создан" });
         }
 
         [HttpPatch("{id}")]
-        public bool UpdateTechnician(int id, string? name, string? phone, string? specialization, DateTime? startWork, DateTime? startWorkInCompany)
+        public ActionResult<Technician> UpdateTechnician(int id, string? name, string? phone, string? specialization, DateTime? startWork, DateTime? startWorkInCompany)
         {
             var updTechnician = _context.Technicians.SingleOrDefault(techi => techi.Id == id);
+
+            var validator = new SymbolValidator(new char[] { '%', '$', '@', '!', '%', '^', '`' });
+            if ((name == null || name.Length == 0) || (phone == null || phone.Length == 0) || (specialization == null || specialization.Length == 0))
+            {
+                return BadRequest(
+                    new { message = "Имя или номер или специализация техника не могут быть пустыми" }
+                );
+            }
+
+
+
+            if (!name.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("Имя техника может содержать только буквы");
+            }
+            if (validator.IsValid(specialization) == false)
+            {
+                return BadRequest("Специализация техника не может содержать такие символы");
+            }
+
+            var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+            try
+            {
+                var phoneNumber = phoneNumberUtil.Parse(phone, "UA");
+                if (!phoneNumberUtil.IsValidNumber(phoneNumber))
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Номер телефона должен быть корректным");
+            }
             if (updTechnician != null)
             {
                 updTechnician.Name = name ?? updTechnician.Name;
@@ -56,13 +125,13 @@ namespace AutoserviceBackCSharp.Controllers
                 }
                 
                 _context.SaveChanges();
-                return true;
+                return Ok(new { updTechnician = updTechnician, message = "Техник успешно обновлен" });
             }
-            return false;
+            return NotFound(new { message = "Техник не найден" }); 
         }
 
         [HttpDelete("{id}")]
-        public bool DeleteTechnician(int id)
+        public ActionResult DeleteTechnician(int id)
         {
             var technician = _context.Technicians.SingleOrDefault(techi => techi.Id == id);
 
@@ -70,10 +139,10 @@ namespace AutoserviceBackCSharp.Controllers
             {
                 _context.Remove(technician);
                 _context.SaveChanges();
-                return true;
+                return Ok(new { message = "Техник успешно ликвидирован" });
             }
 
-            return false;
+            return  NotFound(new { message = "Техник не найден" }); ;
         }
     }
 }
---
+++ AutoserviceBackCSharp/Validation/SymbolValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace AutoserviceBackCSharp.Validation
+{
+    public class SymbolValidator
+    {
+        private readonly char[] _symbolsToCheck;
+
+        public SymbolValidator(char[] symbolsToCheck)
+        {
+            _symbolsToCheck = symbolsToCheck;
+        }
+
+        public bool IsValid(string propertyValue)
+        {
+            if (string.IsNullOrEmpty(propertyValue))
+            {
+                return false;
+            }
+
+            foreach (char symbol in _symbolsToCheck)
+            {
+                if (propertyValue.Contains(symbol))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
