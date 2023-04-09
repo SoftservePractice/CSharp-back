396b8ea9ce9e01cb4b70866ae37081641cd5fe31
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2023-04-09 23:20:15 +0300

    added validation on DetailList controller

--- AutoserviceBackCSharp/Controllers/DetailListController.cs
+++ AutoserviceBackCSharp/Controllers/DetailListController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
+using AutoserviceBackCSharp.Validation;
 
 namespace AutoserviceBackCSharp.Controllers
 {
@@ -36,6 +37,45 @@ namespace AutoserviceBackCSharp.Controllers
         [HttpPost]
         public DetailList PostDetailList(int warehouseId, int detailId, int? count)
         {
+
+            var validator = new SymbolValidator(new char[] { '%', '$', '@', '!', '%', '^', '`' });
+            if ((warehouseId == null || warehouseId.Length == 0) || (detailId == null || detailId.Length == 0)||(count==null||count.Length==0))
+            {
+                return BadRequest(
+                    new { message = "ID склада, ID детали или кол-во деталей не могут быть пустыми" }
+                );
+            }
+
+            if (warehouseId.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("ID склада не может содержать буквы");
+            }
+            if (validator.IsValid(warehouseId)==false)
+            {
+                return BadRequest("ID склада не может содержать такие символы");
+            }
+
+
+            if (detailId.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("ID склада не может содержать буквы");
+            }
+            if (validator.IsValid(detailId)==false)
+            {
+                return BadRequest("ID детали не может содержать такие символы");
+            }
+
+
+            if (count.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("Кол-во деталей не может содержать буквы");
+            }
+            if (validator.IsValid(count)==false)
+            {
+                return BadRequest("Кол-во нне может содержать такие символы");
+            }
+
+
             var newDetailList = new DetailList() { Warehouse = warehouseId, Detail = detailId, Count = count ?? 0 };
             _context.DetailLists.Add(newDetailList);
             _context.SaveChanges();
@@ -45,6 +85,44 @@ namespace AutoserviceBackCSharp.Controllers
         [HttpPatch("{id}")]
         public bool UpdateDetailList(int id, int? warehouseId, int? detailId, int? count)
         {
+            var validator = new SymbolValidator(new char[] { '%', '$', '@', '!', '%', '^', '`' });
+            if ((warehouseId == null || warehouseId.Length == 0) || (detailId == null || detailId.Length == 0)||(count==null||count.Length==0))
+            {
+                return BadRequest(
+                    new { message = "ID склада, ID детали или кол-во деталей не могут быть пустыми" }
+                );
+            }
+
+            if (warehouseId.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("ID склада не может содержать буквы");
+            }
+            if (validator.IsValid(warehouseId)==false)
+            {
+                return BadRequest("ID склада не может содержать такие символы");
+            }
+
+
+            if (detailId.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("ID склада не может содержать буквы");
+            }
+            if (validator.IsValid(detailId)==false)
+            {
+                return BadRequest("ID детали не может содержать такие символы");
+            }
+
+
+            if (count.All(x => char.IsLetter(x)))
+            {
+                return BadRequest("Кол-во деталей не может содержать буквы");
+            }
+            if (validator.IsValid(count)==false)
+            {
+                return BadRequest("Кол-во нне может содержать такие символы");
+            }
+
+
             var updDetailList = _context.DetailLists.SingleOrDefault(detailList => detailList.Id == id);
             if (updDetailList != null)
             {
