74e986df98662882a296d08654c62700a594d5d5
bajenoB <jonjill1488@gmail.com>
2023-04-16 19:31:22 +0300

    Update Work and Techician ModelClass

--- AutoserviceBackCSharp/Models/Work.cs
+++ AutoserviceBackCSharp/Models/Work.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace AutoserviceBackCSharp.Models;
 
@@ -8,12 +9,18 @@ public partial class Work
 {
     public int Id { get; set; }
 
+    [Range(0,int.MaxValue,ErrorMessage = "Value must be non-negative")]
     public int? Detail { get; set; }
 
+    [Range(0, float.MaxValue, ErrorMessage = "Value must be non-negative")]
     public float? DetailPrice { get; set; }
 
+    [Required(ErrorMessage = "Не указана WorkPrice")]
+    [Range(0, float.MaxValue, ErrorMessage = "Value must be non-negative")]
     public float WorkPrice { get; set; }
 
+    [Required(ErrorMessage = "Не указана Order")]
+    [Range(0, int.MaxValue, ErrorMessage = "Value must be non-negative")]
     public int Order { get; set; }
     [JsonIgnore]
     public virtual Detail? DetailNavigation { get; set; }
