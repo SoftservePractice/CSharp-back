b19300611c8751b12625383ebd488c454c246ad4
Prikhod322 <vovkaprikhod@gmail.com>
2023-04-18 00:40:15 +0300

    updated car tests & controller

--- AutoserviceBackUnitTests/ControllersTests/CarTest.cs
+++ AutoserviceBackUnitTests/ControllersTests/CarTest.cs
@@ -1,76 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using AutoserviceBackCSharp.Controllers;
 using AutoserviceBackCSharp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NUnit.Framework.Internal;
 
 namespace AutoserviceBackUnitTests.ControllersTests
 {
     public class CarTest
     {
-        public readonly ILogger<CarController>? logger;
-        public readonly PracticedbContext? context;
-        CarController carController;
+        private CarController carController;
+        private Random random;
 
         [SetUp]
         public void Setup()
         {
-
+            carController = new CarController(null, PublicContext.context);
+            random = new Random();
         }
 
         [Test]
-        public void CarController_Get()
+        public void CarControllerGet_TestNoExceptions()
         {
-            carController = new CarController(logger, context);
-            var result = carController.GetCars();
+            Assert.DoesNotThrow(() => carController.GetCars());
+        }
+        [Test]
+        public void CarControllerGet_TestNoNullRes()
+        {
+            var result = carController.GetCars().Result;
 
-            Assert.Pass("123", result);
+            Assert.IsTrue(result != null);
+        }
+        [Test]
+        public void CarControllerGet_TestIsOkResult()
+        {
+            var result = carController.GetCars().Result;
+
+            Assert.IsTrue(result is OkObjectResult);
+        }
+        [Test]
+        public void CarControllerGet_TestIsTableHasRows()
+        {
+            var result = carController.GetCars().Result;
+            OkObjectResult okObjRes;
+            try
+            {
+                okObjRes = (OkObjectResult)result!;
+                Assert.IsTrue(okObjRes.Value is Array && ((Car[])okObjRes.Value).Length > 0);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
         }
 
-        public void CarController_Get2()
+        [Test]
+        public void CarCarControllerPost_TestNoNullRes()
         {
-            carController = new CarController(logger, context);
-            int id = 1;
-            var result = carController.GetCar(id);
-            Assert.Pass("1233", result);
-        }
-
-        public void CarController_Post()
-        {
-            carController = new CarController(logger, context);
-            string mark = "sse";
-            DateTime year = new DateTime(2008, 3, 1, 7, 0, 0);
-            string vin = "dfs";
-            string carNumber = "ssq";
-            int clientID = 1;
+            string mark = "BMW";
+            DateTime year = DateTime.Now;
+            string vin = random.Next(100000, 999999).ToString();
+            string carNumber = random.Next(100000, 999999).ToString();
+            int clientID = 2;
 
             var result = carController.PostCar(mark, year, vin, carNumber, clientID);
-            Assert.Pass("1234", result);
+            Assert.IsNotNull(result);
         }
 
-        public void CarController_Update()
+        [Test]
+        public void CarCarControllerPost_TestIsCreatedResult()
         {
-            carController = new CarController(logger, context);
-            int id = 1;
-            string mark = "sse";
-            DateTime year = new DateTime(2008, 3, 1, 7, 0, 0);
-            string vin = "dfs";
-            string carNumber = "ssq";
-            int clientID = 1;
+            string mark = "BMW";
+            DateTime year = DateTime.Now;
+            string vin = random.Next(100000, 999999).ToString();
+            string carNumber = random.Next(100000, 999999).ToString();
+            int clientID = 2;
+
+            var result = carController.PostCar(mark, year, vin, carNumber, clientID);
+            Assert.IsTrue((result as CreatedAtActionResult).StatusCode == (int)HttpStatusCode.Created);
+        }
+
+        [Test]
+        public void CarControllerUpdate_TestNoNullResult()
+        {
+            int id = 25;
+            string mark = "BMW";
+            DateTime year = DateTime.Now;
+            string vin = random.Next(100000, 999999).ToString();
+            string carNumber = random.Next(100000, 999999).ToString();
+            int clientID = 2;
+
             var result = carController.UpdateCar(id, mark, year, vin, carNumber, clientID);
-            Assert.Pass("1231", result);
+            Assert.IsNotNull(result);
         }
 
-        public void CarController_Delete()
+        [Test]
+        public void CarControllerUpdate_TestIsOkResult()
         {
-            carController = new CarController(logger, context);
-            int id = 1;
+            int id = 25;
+            string mark = "BMW";
+            DateTime year = DateTime.Now;
+            string vin = random.Next(100000, 999999).ToString();
+            string carNumber = random.Next(100000, 999999).ToString();
+            int clientID = 2;
+
+            var result = carController.UpdateCar(id, mark, year, vin, carNumber, clientID).Result;
+            Assert.IsTrue(result is OkObjectResult);
+        }
+
+        [Test]
+        public void CarControllerDelete_TestNoNullResult()
+        {
+            int id = 32;
+
             var result = carController.DeleteCar(id);
-            Assert.Pass("1231", result);
+            Assert.IsNotNull(result);
+        }
+
+
+        [Test]
+        public void CarControllerDelete_TestIsOkResult()
+        {
+            int id = 33;
+
+            var result = carController.DeleteCar(id);
+            Assert.IsTrue(result is OkObjectResult);
         }
     }
 }
